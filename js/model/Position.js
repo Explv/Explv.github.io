fc60ed670fc311a914156b831aba7b15e3e693cf
Explv <Explv@osbot.org>
2019-06-13 22:07:17 +0100

    Fixing Position toLatLng / fromLatLng

--- js/model/Position.js
+++ js/model/Position.js
@@ -15,21 +15,23 @@ export class Position {
 
     static fromLatLng(map, latLng, z) {
         var point = map.project(latLng, map.getMaxZoom());
-        var y = MAP_HEIGHT_PX - point.y;
+        var y = MAP_HEIGHT_PX - point.y + (RS_TILE_HEIGHT_PX / 4);
         y = Math.round((y - RS_TILE_HEIGHT_PX) / RS_TILE_HEIGHT_PX) + RS_OFFSET_Y;
         var x = Math.round((point.x - RS_TILE_WIDTH_PX) / RS_TILE_WIDTH_PX) + RS_OFFSET_X;
         return new Position(x, y, z);
     }
 
     toLatLng(map) {
-        var x = ((this.x - RS_OFFSET_X) * RS_TILE_WIDTH_PX) + (RS_TILE_WIDTH_PX / 4);
-        var y = (MAP_HEIGHT_PX - ((this.y - RS_OFFSET_Y) * RS_TILE_HEIGHT_PX)) - (RS_TILE_HEIGHT_PX / 4);
-        return map.unproject(L.point(x, y), map.getMaxZoom());
+        return Position.toLatLng(map, this.x, this.y)
     }
 
     toCentreLatLng(map) {
-        var x = ((this.x + 0.5 - RS_OFFSET_X) * RS_TILE_WIDTH_PX) + (RS_TILE_WIDTH_PX / 4);
-        var y = (MAP_HEIGHT_PX - ((this.y + 0.5 - RS_OFFSET_Y) * RS_TILE_HEIGHT_PX)) - (RS_TILE_HEIGHT_PX / 4);
+        return Position.toLatLng(map, this.x + 0.5, this.y + 0.5)
+    }
+
+    static toLatLng(map, x, y) {
+        x = ((x - RS_OFFSET_X) * RS_TILE_WIDTH_PX) + (RS_TILE_WIDTH_PX / 4);
+        y = (MAP_HEIGHT_PX - ((y - RS_OFFSET_Y) * RS_TILE_HEIGHT_PX));
         return map.unproject(L.point(x, y), map.getMaxZoom());
     }
 
@@ -40,13 +42,8 @@ export class Position {
     }
 
     toLeaflet(map) {
-        var point = map.project(this.toLatLng(map), map.getMaxZoom());
-        var startX = (Math.floor(point.x / RS_TILE_WIDTH_PX) * RS_TILE_WIDTH_PX) + (RS_TILE_WIDTH_PX / 4);
-        var startY = (Math.floor(point.y / RS_TILE_HEIGHT_PX) * RS_TILE_HEIGHT_PX);
-        var endX = startX + RS_TILE_WIDTH_PX;
-        var endY = startY + RS_TILE_HEIGHT_PX;
-        var startLatLng = map.unproject(L.point(startX, startY), map.getMaxZoom());
-        var endLatLng = map.unproject(L.point(endX, endY), map.getMaxZoom());
+        var startLatLng = this.toLatLng(map)
+        var endLatLng = new Position(this.x + 1, this.y + 1, this.z).toLatLng(map)
 
         return L.rectangle(L.latLngBounds(startLatLng, endLatLng), {
             color: "#33b5e5",
@@ -66,6 +63,6 @@ export class Position {
     }
 
     toString() {
-        return "(" + this.x + ", " + this.y + ", " + this.z + ")";
+        return `(${this.x}, ${this.y}, ${this.z})`;
     }
 };
