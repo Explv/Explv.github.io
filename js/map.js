c718d3c55091d45c7e5b45355a1b37db6193d75f
Explv <explv@osbot.org>
2020-04-04 11:41:40 +0100

    Add URL params for centre position, region ID and zoom

--- js/map.js
+++ js/map.js
@@ -1,31 +1,41 @@
 'use strict';
 
-import {Position} from './model/Position.js';
+import { Position } from './model/Position.js';
 
 // Import controls
-import {CollectionControl} from './controls/collection_control.js';
-import {CoordinatesControl} from './controls/coordinates_control.js';
-import {LocalCoordinatesControl} from './controls/local_coordinates_control.js';
-import {RegionBaseCoordinatesControl} from './controls/region_base_coordinates_control.js';
-import {GridControl} from './controls/grid_control.js';
-import {LocationLookupControl} from './controls/location_lookup_control.js';
-import {MapLabelControl} from './controls/map_label_control.js';
-import {PlaneControl} from './controls/plane_control.js';
-import {RegionLabelsControl} from './controls/region_labels_control.js';
-import {RegionLookupControl} from './controls/region_lookup_control.js';
-import {TitleLabel} from './controls/title_label.js';
+import { CollectionControl } from './controls/collection_control.js';
+import { CoordinatesControl } from './controls/coordinates_control.js';
+import { LocalCoordinatesControl } from './controls/local_coordinates_control.js';
+import { RegionBaseCoordinatesControl } from './controls/region_base_coordinates_control.js';
+import { GridControl } from './controls/grid_control.js';
+import { LocationLookupControl } from './controls/location_lookup_control.js';
+import { MapLabelControl } from './controls/map_label_control.js';
+import { PlaneControl } from './controls/plane_control.js';
+import { RegionLabelsControl } from './controls/region_labels_control.js';
+import { RegionLookupControl } from './controls/region_lookup_control.js';
+import { TitleLabel } from './controls/title_label.js';
+import { Region } from './model/Region.js';
 
 $(document).ready(function () {
+
+    const currentUrl = new URL(window.location.href);
+
+    const urlCentreX = currentUrl.searchParams.get("centreX");
+    const urlCentreY = currentUrl.searchParams.get("centreY");
+    const urlCentreZ = currentUrl.searchParams.get("centreZ");
+    const urlZoom = currentUrl.searchParams.get("zoom");
+
+    const urlRegionID = currentUrl.searchParams.get("regionID");
 
     var map = L.map('map', {
         //maxBounds: L.latLngBounds(L.latLng(-40, -180), L.latLng(85, 153))
         zoomControl: false,
         renderer: L.canvas()
-    }).setView([-79, -137], 7);
+    });
 
     map.plane = 0;
 
-    map.updateMapPath = function() {
+    map.updateMapPath = function () {
         if (map.tile_layer !== undefined) {
             map.removeLayer(map.tile_layer);
         }
@@ -55,9 +65,9 @@ $(document).ready(function () {
     map.addControl(new RegionLookupControl());
     map.addControl(new GridControl());
     map.addControl(new RegionLabelsControl());
-    
+
     var prevMouseRect, prevMousePos;
-    map.on('mousemove', function(e) {
+    map.on('mousemove', function (e) {
         var mousePos = Position.fromLatLng(map, e.latlng, map.plane);
 
         if (prevMousePos !== mousePos) {
@@ -72,4 +82,35 @@ $(document).ready(function () {
             prevMouseRect.addTo(map);
         }
     });
+
+    const setUrlParams = () => {
+        const mapCentre = map.getBounds().getCenter()
+        const centrePos = Position.fromLatLng(map, mapCentre, map.plane);
+
+        const zoom = map.getZoom();
+
+        window.history.replaceState(null, null, `?centreX=${centrePos.x}&centreY=${centrePos.y}&centreZ=${centrePos.z}&zoom=${zoom}`);
+    };
+
+    map.on('move', setUrlParams);
+    map.on('zoom', setUrlParams);
+
+    let zoom = 7;
+    let centreLatLng = [-79, -137]
+
+    if (urlZoom) {
+        zoom = urlZoom;
+    }
+
+    if (urlCentreX && urlCentreY && urlCentreZ) {
+        const centrePos = new Position(Number(urlCentreX), Number(urlCentreY), Number(urlCentreZ));
+        centreLatLng = centrePos.toLatLng(map);
+    } else if (urlRegionID) {
+        const region = new Region(Number(urlRegionID));
+        const centrePos = region.toCentrePosition()
+        centreLatLng = centrePos.toLatLng(map);
+        zoom = urlZoom || 9;
+    }
+
+    map.setView(centreLatLng, zoom)
 });
