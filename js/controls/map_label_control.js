e34e8dfe6e1f886b290a7e0940c52bd6356039a7
Explv <explv@osbot.org>
2020-12-18 22:38:11 +0000

    Use Verdana font, add more map labels

--- js/controls/map_label_control.js
+++ js/controls/map_label_control.js
@@ -1,8 +1,8 @@
 'use strict';
 
-import {CanvasLayer} from '../external/L.CanvasLayer.js';
+import { CanvasLayer } from '../external/L.CanvasLayer.js';
 import Locations from "../model/Locations.js";
-import {Position} from "../model/Position.js";
+import { Position } from "../model/Position.js";
 
 var MapLabelsCanvas = CanvasLayer.extend({
     setData: function (data) {
@@ -11,13 +11,13 @@ var MapLabelsCanvas = CanvasLayer.extend({
 
     onDrawLayer: function (info) {
         var zoom = this._map.getZoom();
-                
+
         var ctx = info.canvas.getContext('2d');
         ctx.clearRect(0, 0, info.canvas.width, info.canvas.height);
 
         ctx.textAlign = "center";
         var self = this;
-        Locations.getLocations(function(locations) {
+        Locations.getLocations(function (locations) {
             for (var i in locations) {
                 if (locations[i].position.z !== info.layer._map.plane) {
                     continue;
@@ -40,17 +40,17 @@ var MapLabelsCanvas = CanvasLayer.extend({
                         fontSize = 0.18
                         fontColour = '#ffaa00';
                 }
-            
-                // Scale font size to match zoom
-                const fontSizeScaled =  fontSize * Math.pow(2, zoom);
 
-                ctx.font = `bold ${fontSizeScaled}px Arial` 
+                // Scale font size to match zoom
+                const fontSizeScaled = fontSize * Math.pow(2, zoom);
+
+                ctx.font = `bold ${fontSizeScaled}px Verdana`
                 ctx.fillStyle = fontColour
-                
+
                 var position = locations[i].position;
                 var latLng = position.toCentreLatLng(self._map);
                 var canvasPoint = info.layer._map.latLngToContainerPoint(latLng);
-                
+
                 const name = locations[i].name
 
                 const words = name.split(' ')
@@ -92,7 +92,7 @@ export var MapLabelControl = L.Control.extend({
 
     onAdd: function (map) {
         map.createPane("map-labels");
-        
+
         var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control noselect');
         container.style.background = 'none';
         container.style.width = '100px';
@@ -105,20 +105,20 @@ export var MapLabelControl = L.Control.extend({
         L.DomEvent.on(labelsButton, 'click', this._toggleMapLabels, this);
 
         this._enabled = true;
-        
+
         L.DomEvent.disableClickPropagation(container);
 
-        this._mapLabelsCanvas = new MapLabelsCanvas({pane: "map-labels"});
+        this._mapLabelsCanvas = new MapLabelsCanvas({ pane: "map-labels" });
         this._map.addLayer(this._mapLabelsCanvas);
 
-        map.on('planeChanged', function() {
+        map.on('planeChanged', function () {
             this._mapLabelsCanvas.drawLayer();
         }, this);
 
         return container;
     },
 
-    _toggleMapLabels: function() {
+    _toggleMapLabels: function () {
         if (this._enabled) {
             this._map.getPane("map-labels").style.display = "none";
             this._enabled = false;
